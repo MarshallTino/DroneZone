d29dbba5f67b06d115f6fc2a7e7d41a6317cb9d4
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-11 18:16:48 +0100

    Create Layout component

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { showErrorToast } from "../../modals/modals";
+import { useAppSelector } from "../../store/hooks";
 
 const LoginPage = (): JSX.Element => {
+  const { modal } = useAppSelector((state) => state.ui);
+
+  useEffect(() => {
+    if (modal) {
+      showErrorToast(modal);
+    }
+  }, [modal]);
+
   return (
     <>
       <LoginForm />
