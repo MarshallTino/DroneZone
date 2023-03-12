309045d2c82aefa4714acebb7f18669ac5f103d5
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-12 19:28:12 +0100

    Create dronesSlice test and dronesArray for test

---
+++ src/store/features/dronesSlice/dronesSlice.test.ts
@@ -0,0 +1,20 @@
+import { mockedDrones } from "../../../mocks/dronesArray";
+import {
+  dronesInitialState,
+  dronesReducer,
+  loadDronesActionCreator,
+} from "./dronesSlice";
+
+describe("Given a dronesReducer", () => {
+  describe("When its loadDrones action is called passing it a drones array", () => {
+    test("Then the value of the store should contain the array of the drones passed", () => {
+      const loadDronesAction = loadDronesActionCreator(mockedDrones);
+      const newDronesState = dronesReducer(
+        dronesInitialState,
+        loadDronesAction
+      );
+
+      expect(newDronesState).toStrictEqual(mockedDrones);
+    });
+  });
+});
