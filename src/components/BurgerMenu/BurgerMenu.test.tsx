5746bbad11841874aa50056b40b124acf13bfeee
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-15 19:50:40 +0100

    Fix burger menu so it closes when a link is clicked, test it.

--- src/components/BurgerMenu/BurgerMenu.test.tsx
+++ src/components/BurgerMenu/BurgerMenu.test.tsx
@@ -3,6 +3,13 @@ import { renderRouterWithProviders } from "../../utils/testUtils/renderWithProvi
 import BurgerMenu from "./BurgerMenu";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
+import {
+  preloadedStateDrones,
+  preloadedStateLoggedIn,
+} from "../../utils/testUtils/preloadedStates";
+import React from "react";
+
+afterEach(() => jest.clearAllMocks);
 
 describe("Given a BrugerMenu component", () => {
   describe("When it is rendered", () => {
@@ -15,10 +22,11 @@ describe("Given a BrugerMenu component", () => {
   });
 
   describe("When the burger menu is clicked", () => {
-    test("Then it should show a side menu with the title DroneZone", async () => {
-      renderRouterWithProviders({}, <BurgerMenu />);
+    test("Then it should show a side menu with the link Explore Schemas", async () => {
+      renderRouterWithProviders(preloadedStateLoggedIn, <BurgerMenu />);
 
       const burgerButton = screen.getByRole("button", { name: "Open Menu" });
+      await act(async () => await userEvent.click(burgerButton));
       await act(async () => await userEvent.click(burgerButton));
 
       const brugerMenuTitle = screen.getByRole("link", {
@@ -26,6 +34,57 @@ describe("Given a BrugerMenu component", () => {
       });
 
       expect(brugerMenuTitle).toBeInTheDocument();
+    });
+
+    test("Then the setIsOpen should be called with true", async () => {
+      const setIsOpen = jest.fn();
+      jest
+        .spyOn(React, "useState")
+        .mockImplementation(() => [false, setIsOpen]);
+
+      renderRouterWithProviders(
+        { drones: preloadedStateDrones },
+        <BurgerMenu />
+      );
+
+      const burgerButton = screen.getByRole("button", { name: "Open Menu" });
+      await act(async () => await userEvent.click(burgerButton));
+
+      expect(setIsOpen).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe("When the burger menus is opened and the user clicks on the close button", () => {
+    test("Then the setIsOpen should be called with false", async () => {
+      const setIsOpen = jest.fn();
+      jest.spyOn(React, "useState").mockImplementation(() => [true, setIsOpen]);
+
+      renderRouterWithProviders({}, <BurgerMenu />);
+
+      const burgerButton = screen.getByRole("button", { name: "Open Menu" });
+      await act(async () => await userEvent.click(burgerButton));
+      const closButton = screen.getByRole("button", { name: "Close Menu" });
+      await act(async () => await userEvent.click(closButton));
+
+      expect(setIsOpen).toHaveBeenCalledWith(false);
+    });
+  });
+
+  describe("When the burger menus is opened and the user clicks on the link Explore Schemas", () => {
+    test("Then the setIsOpen should be called with false", async () => {
+      const setIsOpen = jest.fn();
+      jest.spyOn(React, "useState").mockImplementation(() => [true, setIsOpen]);
+
+      renderRouterWithProviders({}, <BurgerMenu />);
+
+      const burgerButton = screen.getByRole("button", { name: "Open Menu" });
+      await act(async () => await userEvent.click(burgerButton));
+      const linkExploreSchemas = screen.getByRole("link", {
+        name: "Explore Schemas",
+      });
+      await act(async () => await userEvent.click(linkExploreSchemas));
+
+      expect(setIsOpen).toHaveBeenCalledWith(false);
     });
   });
 });
--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,22 +1,32 @@
+import { useState } from "react";
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import BurgerStyled from "./BurgerMenu.Styled";
 
 const BurgerMenu = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <BurgerStyled>
-      <Menu>
+      <Menu
+        isOpen={isOpen}
+        onOpen={() => setIsOpen(true)}
+        onClose={() => setIsOpen(false)}
+      >
         <div className="menu-header">
           <h2 className="menu-header__title">DroneZone</h2>
         </div>
-        <Link className="menu-item" to={"/"}>
+        <Link className="menu-item" to={"/"} onClick={closeMenu}>
           Explore Schemas
         </Link>
-        <Link className="menu-item" to={"/"}>
+        <Link className="menu-item" to={"/"} onClick={closeMenu}>
           Create a Schema
         </Link>
-        <Link className="menu-item" to={"/myProfile"}>
+        <Link className="menu-item" to={"/myProfile"} onClick={closeMenu}>
           My Profile
         </Link>
         <Button
