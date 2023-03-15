3381a656f45adae38bf0dd13223f951d411bf371
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-15 19:50:09 +0100

    Style myProfilePage and test it

--- src/pages/myProfilePage/myProfilePage.tsx
+++ src/pages/myProfilePage/myProfilePage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import DronesList from "../../components/DronesList/DronesList";
 import useDrones from "../../hooks/useDrones/useDrones";
+import MyProfilePageStyled from "./myProfilePageStyled";
 
 const MyProfilePage = (): JSX.Element => {
   const { getUserDrones } = useDrones();
@@ -9,7 +10,14 @@ const MyProfilePage = (): JSX.Element => {
     getUserDrones();
   }, [getUserDrones]);
 
-  return <DronesList />;
+  return (
+    <MyProfilePageStyled>
+      <div className="profile-page__title-container">
+        <h2 className="profile-page__title">My Drone Schemas</h2>
+      </div>
+      <DronesList />
+    </MyProfilePageStyled>
+  );
 };
 
 export default MyProfilePage;
---
+++ src/pages/myProfilePage/myProfilePageStyled.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const MyProfilePageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 30px;
+
+  .profile-page__title-container {
+    background-color: ${(props) => props.theme.colors.mainDark};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0px 15px;
+    height: 69px;
+    border-radius: 5px;
+  }
+  .profile-page__title {
+    padding-top: 15px;
+    color: ${(props) => props.theme.colors.mainLight};
+    font-size: 28px;
+    height: 100%;
+    width: 100%;
+    font-weight: 500;
+    position: relative;
+    text-align: center;
+    border-bottom: 3px solid ${(props) => props.theme.colors.base};
+  }
+`;
+export default MyProfilePageStyled;
