f92875f36e51acd461f8d6995be7d38ff1a114bf
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-16 16:37:44 +0100

    Edit drone card to render Delete button if user is creator. Test it.

--- src/components/DroneCard/DroneCard.test.tsx
+++ src/components/DroneCard/DroneCard.test.tsx
@@ -2,7 +2,26 @@ import { screen } from "@testing-library/react";
 import { mockedDrone } from "../../mocks/dronesArray";
 import { DroneCard } from "./DroneCard";
 import "@testing-library/jest-dom";
-import { renderWithProviders } from "../../utils/testUtils/renderWithProviders";
+import userEvent from "@testing-library/user-event";
+import {
+  renderRouterWithProviders,
+  renderWithProviders,
+} from "../../utils/testUtils/renderWithProviders";
+import { act } from "react-dom/test-utils";
+
+beforeAll(() => {
+  jest.clearAllMocks();
+  jest.clearAllTimers();
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+const mockDeleteDrone = jest.fn();
+
+jest.mock("../../hooks/useDrones/useDrones", () => () => ({
+  deleteDrone: mockDeleteDrone,
+}));
 
 describe("Given a DroneCard component", () => {
   describe("When it receives a drone and is rendered", () => {
@@ -20,6 +39,27 @@ describe("Given a DroneCard component", () => {
         name: "Transmission",
       });
       expect(transmission).toBeInTheDocument();
+    });
+  });
+
+  describe("When it receives a drone created by that user and the Delete button is clicked", () => {
+    test("Then the deleteDrone function should be called", async () => {
+      renderRouterWithProviders(
+        {
+          user: {
+            email: "",
+            id: "640729e2304c8728da0a679a",
+            isLogged: true,
+            token: "",
+          },
+        },
+        <DroneCard drone={mockedDrone} />
+      );
+
+      const deleteButton = screen.getByRole("button", { name: "Delete" });
+      await act(async () => await userEvent.click(deleteButton));
+
+      expect(mockDeleteDrone).toHaveBeenCalled();
     });
   });
 });
--- src/components/DroneCard/DroneCard.tsx
+++ src/components/DroneCard/DroneCard.tsx
@@ -1,40 +1,55 @@
+import useDrones from "../../hooks/useDrones/useDrones";
 import { DroneStructure } from "../../store/features/dronesSlice/types";
+import { useAppSelector } from "../../store/hooks";
+import Button from "../Button/Button";
 import DroneCardStyled from "./DroneCardStyled";
 
 export interface DroneCardProps {
   drone: DroneStructure;
 }
 
-export const DroneCard = ({
-  drone: { droneImage, categories, creator },
-}: DroneCardProps): JSX.Element => {
+export const DroneCard = ({ drone }: DroneCardProps): JSX.Element => {
+  const { deleteDrone } = useDrones();
+  const userID = useAppSelector((state) => state.user.id);
+  const isMyDrone = userID === drone.creator;
   return (
     <DroneCardStyled className="drone-card">
       <img
         className="drone-card__image"
         width="350"
         height="320"
-        src={droneImage}
+        src={drone.droneImage}
         alt={"drone"}
       />
+
+      {isMyDrone && (
+        <Button
+          className="drone-card__delete"
+          text={"Delete"}
+          disabled={false}
+          action={() => deleteDrone(drone)}
+        />
+      )}
       <div className="properties">
         <span className="properties__container">
           <h2 className="properties__title">Level</h2>
-          <h3 className="properties__value">{categories.difficulty}</h3>
+          <h3 className="properties__value">{drone.categories.difficulty}</h3>
         </span>
         <span className="properties__container">
           <h2 className="properties__title">Class</h2>
-          <h3 className="properties__value">{categories.droneClass}</h3>
+          <h3 className="properties__value">{drone.categories.droneClass}</h3>
         </span>
 
         <span className="properties__container">
           <h1 className="properties__title">Transmission</h1>
-          <h2 className="properties__value">{categories.transmissionType}</h2>
+          <h2 className="properties__value">
+            {drone.categories.transmissionType}
+          </h2>
         </span>
       </div>
       <div className="creator">
         <h2 className="creator__title">Created by </h2>
-        <h3 className="creator__name"> {creator}</h3>
+        <h3 className="creator__name"> {drone.creatorName}</h3>
       </div>
     </DroneCardStyled>
   );
