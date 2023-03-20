53fa1cd440d9121693737988c2b06f6313b91e9f
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-20 13:09:20 +0100

    Create createDronePage test it and style it

---
+++ src/pages/homePage/createDronePage/createDronePage.test.tsx
@@ -0,0 +1,23 @@
+import { screen } from "@testing-library/react";
+
+import "@testing-library/jest-dom";
+import CreateDronePage from "./createDronePage";
+import {
+  preloadedStateDrones,
+  preloadedStateLoggedIn,
+} from "../../../utils/testUtils/preloadedStates";
+import { renderRouterWithProviders } from "../../../utils/testUtils/renderWithProviders";
+
+describe("Given a createDronePAge", () => {
+  describe("When it is rendered", () => {
+    test("Then it should a form", () => {
+      renderRouterWithProviders(
+        { drones: preloadedStateDrones, ...preloadedStateLoggedIn },
+        <CreateDronePage />
+      );
+      const form = screen.getByLabelText("Motor Name");
+
+      expect(form).toBeInTheDocument();
+    });
+  });
+});
