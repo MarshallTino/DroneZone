55a545ccc7ad79cf9b0163a9352f28721cd07fa4
Marcel Martino <marcelmartino2053@gmail.com>
2023-03-21 14:56:13 +0100

    Create DroneComponentCard and style it

---
+++ src/components/droneComponentCard/droneComponentCardStyled.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const DroneComponentCardStyled = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  padding: 15px;
+  gap: 15px;
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.mainDark};
+  border-radius: 8px;
+
+  .component-card {
+    &__image {
+      object-fit: cover;
+      border-radius: 8px;
+    }
+
+    &__info {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      gap: 8px;
+      align-items: center;
+    }
+  }
+  .info__title {
+    width: 100%;
+    padding: 4px 12px;
+    background-color: ${(props) => props.theme.colors.base};
+    border-radius: 8px;
+    color: ${(props) => props.theme.colors.mainLight};
+    text-align: center;
+  }
+`;
+export default DroneComponentCardStyled;
